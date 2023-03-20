a210b61fab276c86c358e6fa4747cc896e0427cb
mathst <matheusemox@gmail.com>
2023-03-20 09:55:47 -0300

    alterações

--- api/mainApi/models.py
+++ api/mainApi/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 
-
-class Veiculo(models.Model):
-    
-    id = models.BigAutoField(primary_key=True)
-    veiculo = models.CharField(max_length = 180)
-    marca = models.CharField(max_length = 180)
-    cor = models.CharField(max_length = 180)
-    ano = models.PositiveIntegerField()
-    descricao = models.TextField()
-    vendido = models.BooleanField(default = False)
-    created = models.DateTimeField(auto_created= True, blank = True, auto_now_add=True)
-    update = models.DateTimeField(auto_now = True, blank = True)
+class Pedido(models.Model):
+    cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_pedido = models.DateTimeField(auto_now_add=True)
+    itens = models.ManyToManyField(Item, through='PedidoItem')
+    pagamento = models.ForeignKey(Pagamento, on_delete=models.CASCADE)
+    endereco_entrega = models.ForeignKey(Endereco, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.veiculo  
+        return f'Pedido número {self.pk} do cliente {self.cliente}'
+    from django.db import models
+
+class ItemPrincipal(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+    descricao = models.TextField()
+    disponibilidade = models.BooleanField(default=True)
+
+class Pedido(models.Model):
+    itens_principais = models.ManyToManyField(ItemPrincipal)
+    cliente = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_hora = models.DateTimeField(auto_now_add=True)
+    pagamento_efetuado = models.BooleanField(default=False)
